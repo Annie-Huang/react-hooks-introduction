fec39e4639e27de4438ca1b86fc9bcb3d960bd9a
Annie Huang <annie.hn@gmail.com>
2019-04-07 23:14:07 +1000

    (Cannot compile yet) Move componentDidMount logic into useEffect

--- src/components/CharPicker.js
+++ src/components/CharPicker.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
 
 import './CharPicker.css';
 
@@ -8,29 +8,52 @@ const CharPicker = props => {
   const [loadedChars, setLoadedChars] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  componentDidMount() {
+  // componentDidMount() {
+  //   this.setState({ isLoading: true });
+  //   fetch('https://swapi.co/api/people')
+  //     .then(response => {
+  //       if (!response.ok) {
+  //         throw new Error('Failed to fetch.');
+  //       }
+  //       return response.json();
+  //     })
+  //     .then(charData => {
+  //       const selectedCharacters = charData.results.slice(0, 5);
+  //       this.setState({
+  //         characters: selectedCharacters.map((char, index) => ({
+  //           name: char.name,
+  //           id: index + 1
+  //         })),
+  //         isLoading: false
+  //       });
+  //     })
+  //     .catch(err => {
+  //       console.log(err);
+  //     });
+  // }
+  useEffect(() => {
     this.setState({ isLoading: true });
     fetch('https://swapi.co/api/people')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Failed to fetch.');
-        }
-        return response.json();
-      })
-      .then(charData => {
-        const selectedCharacters = charData.results.slice(0, 5);
-        this.setState({
-          characters: selectedCharacters.map((char, index) => ({
-            name: char.name,
-            id: index + 1
-          })),
-          isLoading: false
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Failed to fetch.');
+          }
+          return response.json();
+        })
+        .then(charData => {
+          const selectedCharacters = charData.results.slice(0, 5);
+          this.setState({
+            characters: selectedCharacters.map((char, index) => ({
+              name: char.name,
+              id: index + 1
+            })),
+            isLoading: false
+          });
+        })
+        .catch(err => {
+          console.log(err);
         });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
+  });
 
   // render() {
   //   let content = <p>Loading characters...</p>;

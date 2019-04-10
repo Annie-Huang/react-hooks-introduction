5b0b25963d336117882d7323f9d430ef231cfb9f
Annie Huang <annie.hn@gmail.com>
2019-04-10 20:20:03 +1000

    update notes

--- src/components/CharPicker.js
+++ src/components/CharPicker.js
@@ -63,6 +63,13 @@ const CharPicker = props => {
           setIsLoading(false);
         });
   }, []);
+  // https://reactjs.org/docs/hooks-effect.html
+    // If you want to run an effect and clean it up only once (on mount and unmount), you can pass an empty array ([]) as a second argument.
+    // This tells React that your effect doesn’t depend on any values from props or state, so it never needs to re-run.
+    // This isn’t handled as a special case — it follows directly from how the inputs array always works.
+    // If you pass an empty array ([]), the props and state inside the effect will always have their initial values.
+    // While passing [] as the second argument is closer to the familiar componentDidMount and componentWillUnmount mental model
+
 
   // render() {
   //   let content = <p>Loading characters...</p>;
